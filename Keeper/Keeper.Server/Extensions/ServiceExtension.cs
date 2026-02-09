5bfddde09108c55db6412ebef9a65866b6aeb887
LastJayG <146539704+LastJayG@users.noreply.github.com>
2026-02-09 15:55:13 +0300

    Added Swagger + isRunOnAspire setting

---
+++ Keeper/Keeper.Server/Extensions/ServiceExtension.cs
@@ -0,0 +1,12 @@
+﻿using Keeper.Data.Context;
+
+namespace Keeper.Server.Extensions;
+
+public static class ServiceExtension
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddDbContext<KeeperDbContext>();
+        return services;
+    }
+}

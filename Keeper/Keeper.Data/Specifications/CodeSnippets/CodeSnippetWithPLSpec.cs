5921192935a7cc861e4100a7a2a14f484b2367e3
LastJayG <146539704+LastJayG@users.noreply.github.com>
2026-02-09 17:01:03 +0300

    Added specifications for CodeSnippet

---
+++ Keeper/Keeper.Data/Specifications/CodeSnippets/CodeSnippetWithPLSpec.cs
@@ -0,0 +1,22 @@
+﻿using Keeper.Core.Entities;
+using Keeper.Core.Enums;
+
+namespace Keeper.Data.Specifications.CodeSnippets;
+
+/// <summary>
+/// Takes CodeSnippets with exact ProgrammingLanguage, OrderBy length
+/// </summary>
+public class CodeSnippetWithPLSpec : BaseSpecification<CodeSnippetEntity>
+{
+    public CodeSnippetWithPLSpec(ProgrammingLanguage programmingLanguage)
+    {
+        Criteria = cs => cs.ProgrammingLanguage == programmingLanguage;
+        OrderBy = cs => cs.Code.Length;
+    }
+
+    public CodeSnippetWithPLSpec(ProgrammingLanguage programmingLanguage, int pageNumber, int pageSize) : this(programmingLanguage)
+    {
+        int skip = (pageSize - 1) * pageSize;
+        ApplyPaging(skip, pageSize);
+    }
+}
---
+++ Keeper/Keeper.Data/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Keeper.Data.Specifications;
+
+public class SpecificationEvaluator<T> where T : class
+{
+    public IQueryable<T> GetQuery(IQueryable<T> inputQuery, BaseSpecification<T> spec)
+    {
+        var query = inputQuery;
+
+        if (spec.Criteria != null)
+            query = query.Where(spec.Criteria);
+
+        foreach (var include in spec.Includes)
+            query = query.Include(include);
+
+        if (spec.OrderBy != null)
+            query = query.OrderBy(spec.OrderBy);
+
+        if (spec.IsPagingEnabled)
+            query = query.Skip(spec.Skip).Take(spec.Take);
+
+        return query; 
+    }
+}

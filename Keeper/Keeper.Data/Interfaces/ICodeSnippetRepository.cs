9764aff0368d26a1579f7f8523055f0530d1c11b
LastJayG <146539704+LastJayG@users.noreply.github.com>
2026-02-11 17:48:00 +0300

    Refactored CodeSnippetRepository

---
+++ Keeper/Keeper.Data/Interfaces/ICodeSnippetRepository.cs
@@ -0,0 +1,14 @@
+﻿using Keeper.Core.Entities;
+using Keeper.Data.Specifications;
+
+namespace Keeper.Data.Interfaces;
+public interface ICodeSnippetRepository
+{
+    Task<CodeSnippetEntity> CreateAsync(CodeSnippetEntity entity);
+    Task<int> CountAsync(BaseSpecification<CodeSnippetEntity> spec);
+    Task DeleteAsync(CodeSnippetEntity entity);
+    Task<IReadOnlyList<CodeSnippetEntity>> GetAllAsync();
+    Task<IReadOnlyList<CodeSnippetEntity>> GetAsync(BaseSpecification<CodeSnippetEntity> spec);
+    Task<CodeSnippetEntity> GetByIdAsync(int id);
+    Task<CodeSnippetEntity> UpdateAsync(CodeSnippetEntity entity);
+}

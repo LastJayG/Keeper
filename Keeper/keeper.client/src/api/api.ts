2de163d0430fc7ceea45fcff32056db1527ca59a
LastJayG <146539704+LastJayG@users.noreply.github.com>
2026-02-18 10:30:41 +0300

    Added API + model

---
+++ Keeper/keeper.client/src/api/api.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { FolderDto } from '../models/folder';
+const API_URL = 'https://localhost:7254';
+
+const apiClient = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const api = {
+  async getFolders() {
+    try {
+    const response = await apiClient.get<FolderDto[]>('api/Folder');
+    return response.data;
+    }
+    catch (error:any) {
+        throw error;
+    }
+  }
+};
---
+++ Keeper/keeper.client/src/models/folder.ts
@@ -0,0 +1,6 @@
+export interface FolderDto {
+  id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+}
